885dad4a59d7334b59c5c3487d18fc787c884484
HarshDev1809 <harsh.dev182000@gmail.com>
2024-12-30 02:06:32 +0530

    api bug fix

--- src/controller/auth/isUsernameAvailable.controller.js
+++ src/controller/auth/isUsernameAvailable.controller.js
@@ -1,19 +1,20 @@
+const { failureResponse, successResponse, serverErrorResponse } = require('../../../utils/responseHandler');
 const User = require('../../model/user.model');
 const isUsernameAvailableController = async(req,res)=>{
     const {username} = req.body;
     if(!username){
-        return res.status(400).json({status : 'failure', message : "`Username is not provided`."});
+        return res.status(400).json(failureResponse("Username not provided."));
     }
 
     try{
         const user = await User.find({username : username});
         if(user.length){
-            return res.status(400).json({status : 'successful', available : false, message : "Username Already Taken."});
+            return res.status(200).json(successResponse("Username already taken.",{available : false}));
         }else{
-            return res.status(200).json({status : 'successful', available : true,message : "Username Available."});
+            return res.status(200).json(successResponse("Username Available",{available : true}));
         }
     }catch(err){
-        return res.status(500).json({error: `Internal Server Error`});
+        return res.status(500).json(serverErrorResponse(err.message));
     }
 }
 
