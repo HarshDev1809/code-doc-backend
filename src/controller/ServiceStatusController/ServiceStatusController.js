60c7ac94f8dce174499b4054f0e66ea6c5b53c38
HarshDev1809 <harsh.dev182000@gmail.com>
2025-01-12 13:27:55 +0530

    Service Status added

---
+++ src/controller/ServiceStatusController/ServiceStatusController.js
@@ -0,0 +1,5 @@
+const serviceStatusController = async(req,res)=>{
+    return res.status(200).send("Service Online!_____\\(=^_^=)/")
+}
+
+module.exports = serviceStatusController;
--- src/route/public.route.js
+++ src/route/public.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { isUsernameAvailableController } = require('../controller/auth/isUsernameAvailable.controller');
 const { isEmailAvailableController } = require('../controller/auth/isEmailAvailable.controller');
+const serviceStatusController = require('../controller/ServiceStatusController/ServiceStatusController');
 const publicRoutes = express.Router();
 
+publicRoutes.get('/',serviceStatusController)
 publicRoutes.post('/isAvailable/username',isUsernameAvailableController);
 publicRoutes.post('/isAvailable/email',isEmailAvailableController);
 
